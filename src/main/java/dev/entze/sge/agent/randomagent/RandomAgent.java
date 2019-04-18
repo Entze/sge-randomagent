1bff89d4e8755f8f3654edf3471518b2566599eb
Lukas Grassauer <lukas@grassauer.eu>
2019-04-18 21:59:14 +0200

    make randomagent simpler and add log

--- src/main/java/dev/entze/sge/agent/randomagent/RandomAgent.java
+++ src/main/java/dev/entze/sge/agent/randomagent/RandomAgent.java
@@ -1,28 +1,34 @@
 package dev.entze.sge.agent.randomagent;
 
 import dev.entze.sge.agent.GameAgent;
+import dev.entze.sge.engine.Logger;
 import dev.entze.sge.game.Game;
-import java.util.ArrayList;
-import java.util.List;
+import dev.entze.sge.util.Util;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class RandomAgent<G extends Game<A, ?>, A> implements GameAgent<G, A> {
 
+  private final Logger log;
   private final Random random;
 
+
   public RandomAgent() {
-    this(new Random());
+    this(new Random(), null);
   }
 
-  public RandomAgent(Random random) {
+  public RandomAgent(Logger log) {
+    this(new Random(), log);
+  }
+
+  public RandomAgent(Random random, Logger log) {
     this.random = random;
+    this.log = log;
   }
 
   @Override
   public A computeNextAction(G game, long computationTime, TimeUnit timeUnit) {
-    List<A> possibleActions = new ArrayList<>(game.getPossibleActions());
-    return possibleActions.get(random.nextInt(possibleActions.size()));
+    return Util.selectRandom(game.getPossibleActions(), random);
   }
 
   @Override
